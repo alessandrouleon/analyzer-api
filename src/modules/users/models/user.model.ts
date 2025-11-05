f64b3f4b0c7dd9f3e47e482c540459ac2ac8d3f1
alessandro uleon <alessandrouleon.dev@gmail.com>
2025-11-04 22:03:22 -0400

    create model user

---
+++ src/modules/users/models/user.model.ts
@@ -0,0 +1,34 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+import { BaseSchema } from '../../../@shared/repository/base-model';
+
+export type UserDocument = User & Document;
+
+export enum UserRole {
+  ADMIN = 'admin',
+  USER = 'user',
+}
+
+@Schema()
+export class User extends BaseSchema {
+  @Prop({
+    type: String,
+    required: true,
+    trim: true,
+  })
+  name: string;
+
+  @Prop({ type: String, required: false,   unique: true, trim: true })
+  username: string;
+
+  @Prop({ type: String, required: false })
+  email?: string;
+
+  @Prop({ type: String, required: false, trim: true })
+  password: string;
+
+  @Prop({ type: String, required: false, trim: true, enum: Object.values(UserRole) })
+  role: UserRole;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
